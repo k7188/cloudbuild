1e7fbf3d69e8c60b17116e343dc1c1c25f984d0d
Syed Hussain <hussain.k7188@gmail.com>
2025-08-29 12:14:18 -0400

    finish full dev/prod setup

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -20,30 +20,3 @@ resource "google_storage_bucket" "test_prod_bucket" {
 resource "random_id" "suffix" {
   byte_length = 4
 }
-resource "google_compute_instance" "prod_vm" {
-  name         = "prod-vm"
-  machine_type = "e2-small"  # Slightly larger for production
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      size  = 30  # Larger disk for production
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  tags = ["http-server", "https-server"]
-
-  labels = {
-    environment = "prod"
-    owner       = "operations-team"
-    cost-center = "production"
-  }
-}
